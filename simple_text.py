2af8765a8a9cb457ae5f566a9a7e624fd0ac69bb
Hyunji Kim <hji.kim@megazone.com>
2024-08-23 15:58:55 -0400

    Finished up to finding the cosine similarity.

--- simple_text.py
+++ simple_text.py
@@ -1,6 +1,7 @@
 import nltk
 from nltk.corpus import wordnet as wn
-from viz import Visualization
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
 
 ABB = {'JJ': 'a', 'JJR': 'a', 'JJS': 'a',
        'NN': 'n', 'NNS': 'n', 'NNP': 'n', 'NNPS': 'n',
@@ -11,53 +12,67 @@ ABB = {'JJ': 'a', 'JJR': 'a', 'JJS': 'a',
        'CC': 'c', 'UH': 'i', 'MD': 'm', 'TO': 't', 'EX': 'e', 'SYM': 's'
     }
 
-class Person:
-    def __init__(self, name):
-        self.name = name
-
-    def print_name(self):
-        print(f"{self.name}")
+model = SentenceTransformer('bert-base-nli-mean-tokens')
 
 class Syns:
 
-    def get_synonyms(sentence, word_list):
-        syn_dict = {}   # initialize ('pos' - str, 'synonyms' - list)
-        pos = ''
-        tokens = nltk.word_tokenize(sentence)   # tokenize
-        pos_tags = nltk.pos_tag(tokens)
-        for pos_tag in pos_tags:    # part-of-speech tagging
-            if pos_tag[0] in word_list:
-                pos = ABB[pos_tag[1]]
-                syn_dict[pos_tag[0]] = {}   # initialize
-                syn_dict[pos_tag[0]]['pos'] = pos
+    def extract_lines(paragraph: str, word_list: list) -> dict:
+        word_dict = dict()
+        sents = paragraph.split('.')
         for word in word_list:
+            word_dict[word] = {}    # initiazlize
+            if any(word in sent for sent in sents):
+                index = [idx for idx, sent in enumerate(sents) if word in sent][0]
+                word_dict[word]['sentence'] = sents[index]
+        return word_dict
+
+    def get_synonyms(word_dict: dict) -> dict:
+        pos = ''
+        for word in word_dict:
+            sentence = word_dict[word]['sentence']
+            tokens = nltk.word_tokenize(sentence)   # tokenize
+            pos_tags = nltk.pos_tag(tokens)
+            for pos_tag in pos_tags:    # part-of-speech tagging
+                if pos_tag[0] == word:
+                    pos = ABB[pos_tag[1]]
+                    word_dict[word]['pos'] = pos
             try:
-                synsets = wn.synsets(word, pos=syn_dict[word]['pos'])   # synonyms
-                syn_dict[word]['synonyms'] = [] # initialize
+                synsets = wn.synsets(word, pos=word_dict[word]['pos'])   # synonyms
+                word_dict[word]['synonyms'] = {} # initialize
                 for syn in synsets:
                     for lemma in syn.lemmas():  # lemmas
-                        if lemma.name() not in syn_dict[word]['synonyms']:
-                            syn_dict[word]['synonyms'].append(lemma.name())
+                        if lemma.name() not in word_dict[word]['synonyms']:
+                            word_dict[word]['synonyms'][lemma.name()] = float(0)
             except Exception as e:
                 print(f"An error occurred: {e}.")
-        return syn_dict
+        return word_dict
+    
+class Similarity:
 
+    def cos_sim(word_dict: dict) -> dict:
+        for word in word_list:
+            origin_sent_emb = model.encode(word_dict[word]['sentence'])
+            for syn in word_dict[word]['synonyms']:
+                new_sent = word_dict[word]['sentence'].replace(word, syn)
+                new_embed = model.encode(new_sent)
+                similarity_score = cosine_similarity([origin_sent_emb], [new_embed])
+                word_dict[word]['synonyms'][syn] = similarity_score
+        return word_dict
+    
 
 if __name__=='__main__':
-    hyunji = Person("hyunji")
-    #hyunji.print_name()
-
-    sentence1 = 'She always wins, it is not fair.'
-    word1 = ['wins', 'fair']
-    dict1 = Syns.get_synonyms(sentence1, word1)
-    for word in dict1:
-        print(f"{word}:\npos: {dict1[word]['pos']}\nsynonyms: {dict1[word]['synonyms']}\n")
-
-    sentence2 = '''In fact, however, though now much farther off than before, the Rostóvs
-                all saw Pierre—or someone extraordinarily like him—in a coachman’s coat,
-                going down the street with head bent and a serious face beside a small,
-                beardless old man who looked like a footman.'''
-    word2 = ['serious', 'beardless']
-    dict2 = Syns.get_synonyms(sentence2, word2)
-    for word in dict2:
-        print(f"{word}:\npos: {dict2[word]['pos']}\nsynonyms: {dict2[word]['synonyms']}\n")
+    paragraph = '''Automatic readability assessment (ARA) is the task of evaluating the level of ease or
+                difficulty of text documents for a target audience. For researchers, one of the many
+                open problems in the field is to make such models trained for the task show efficacy
+                even for low-resource languages. In this study, we propose an alternative way of utilizing
+                the information-rich embeddings of BERT models with handcrafted linguistic features through a
+                combined method for readability assessment. Results show that the proposed method outperforms
+                classical approaches in readability assessment.'''
+    word_list = ['evaluating', 'linguistic']
+    
+    word_dict = Syns.extract_lines(paragraph, word_list)
+    word_dict = Syns.get_synonyms(word_dict)
+    word_dict = Similarity.cos_sim(word_dict)
+    for word in word_dict:
+        for syn in word_dict[word]['synonyms']:
+            print(f"Word: '{word}'\nPOS: {word_dict[word]['pos']}\nsynonyms: '{syn}'\nCosine Similarity Score: {word_dict[word]['synonyms'][syn]}\n")
